a0931697428385c550913122be5625b8d4b07f00
tamtan <vinhtam92@gmail.com>
2019-07-30 21:19:43 -0500

    - w2d2 js practice

---
+++ src/W2D2/JSPractice.js
@@ -0,0 +1,27 @@
+// protected namespace
+// use strict
+// well formatted code with short comment on each function or method
+
+function myFunctionTest(expected, found) {
+    if (expected === found) {
+        return "TEST SUCCEEDED";
+    } else {
+        return "TEST FAILED.  Expected " + expected + " found " + found;
+    }
+}
+
+// console.log("Expected output of max(30,6) is 30 " + myFunctionTest(30, max(30, 6)));
+
+// Problem 1: Reverse an Array
+function reverseArray(arr) {
+
+}
+
+function reverseArrayInPlace(arr) {
+
+}
+
+// Problem 2: A List
+
+
+//Problem 3: Deep comparison
