6712346e7ed4bffd3fdec5b13631de7431fe2a54
tamtan <vinhtam92@gmail.com>
2019-07-30 21:43:43 -0500

    - implement functions

--- src/W2D2/JSPractice.js
+++ src/W2D2/JSPractice.js
@@ -14,14 +14,72 @@ function myFunctionTest(expected, found) {
 
 // Problem 1: Reverse an Array
 function reverseArray(arr) {
-
+    let reversed = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed;
 }
 
 function reverseArrayInPlace(arr) {
+    let count = 0;
+    for (let i = 0; i <= Math.floor(arr.length / 2); i++) {
+        var ascendingEl = arr[i];
 
+        arr[i] = arr[arr.length - 1 - i]
+        arr[arr.length - 1 - i] = ascendingEl;
+    }
+    return arr;
 }
 
 // Problem 2: A List
+var list = {
+    value: 1,
+    rest: {
+        value: 2,
+        rest: {
+            value: 3,
+            rest: null
+        }
+    }
+};
 
+arrayToList = (arr) => {
+    let list = null;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        list = {
+            value: arr[i],
+            rest: list
+        }
+    }
+    return list;
+}
+
+listToArray = (list) => {
+    let arr = [];
+    for (const prop in list) {
+        const value = list[prop];
+        typeof value === 'object' ? arr.push(listToArray(value)) : arr.push(value);
+    }
+    arr = [].concat.apply([], arr);
+    return arr;
+}
+
+prepend = (value, rest) => {
+    let list = {
+        value: value,
+        rest: rest,
+    }
+    return list;
+}
+
+nth = (list, num) => {
+    let answer;
+    return num === 0 ? (list.value) : (nth(list.rest, num - 1));
+}
 
 //Problem 3: Deep comparison
+deepEqual = (obj1, obj2) => {
+    return JSON.stringify(obj1) === JSON.stringify(obj2) ? true : false;
+}
+
